451742b3e7ac07292a62ddd471463c0b4d452df4
Adam Possing <atom@possing.com>
2018-11-01 15:17:05 -0700

    Basic Files

---
+++ ProjectEuler/ProjectEuler/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ProjectEuler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+
+
+
+
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.Elapsed);
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
